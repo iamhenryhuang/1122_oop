8c356d7fb1d8e19d80c213df3d6d9c290b686b12
Henry Huang <136736359+iamhenryhuang@users.noreply.github.com>
2024-05-29 20:18:02 +0800

    Create positionstate.h

---
+++ Assign6/funtions/position/positionstate.h
@@ -0,0 +1,16 @@
+#ifndef POSITIONSTATE_H
+#define POSITIONSTATE_H
+
+namespace position
+{
+    enum Direction {
+        DIRECTION_NONE,
+        DIRECTION_UP,
+        DIRECTION_DOWN,
+        DIRECTION_LEFT,
+        DIRECTION_RIGHT
+    };
+} // namespace position
+
+
+#endif
